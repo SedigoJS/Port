ef4621bcfa4f5a85528e9e66d6794e491c5afa8f
Joshua Sedigo <sedigojs@gmail.com>
2025-01-13 00:58:06 +0800

    personal info

--- app/about/AboutContent.tsx
+++ app/about/AboutContent.tsx
@@ -1,38 +1,77 @@
 import Image from 'next/image'
+import { Mail, Phone, MapPin, Cake } from 'lucide-react'
 
-const skills = ['HTML', 'CSS', 'React', 'Tailwind CSS', 'Node.js', 'Next.js', 'TypeScript', 'JavaScript', 'MongoDB', 'PostgreSQL', 'MySQL', 'Prisma ORM', 'AWS'];
+const skills = ['HTML', 'CSS', 'React', 'Tailwind CSS', 'Node.js', 'Next.js', 'TypeScript', 'JavaScript', 'MongoDB', 'PostgreSQL', 'MySQL', 'Prisma ORM', 'AWS']
 
 export default function AboutContent() {
   return (
-    <section className="py-20 dark:bg-none dark:bg-gray-800">
+    <section className="py-20 dark:bg-gray-800">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white mb-8">About Me</h2>
-        <div className="flex flex-col md:flex-row items-center md:items-start space-y-8 md:space-y-0 md:space-x-12">
-          <div className="w-48 h-48 relative">
-            <Image
-              src="/prof.png"
-              alt="Profile picture"
-              layout="fill"
-              objectFit="cover"
-              className="rounded-full"
-            />
+        <div className="flex flex-col items-center md:items-start space-y-8 md:space-y-0 md:space-x-12">
+          <div className='flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-12'>
+            <div className="w-48 h-48 relative ml-0 md:ml-20">
+              <Image
+                src="/prof.png"
+                alt="Profile picture"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-full"
+              />
+            </div>
+            <div className=" w-full md:w-2/3 p-6">
+              <div className="grid grid-cols-2 gap-6">
+                <div className="flex items-center space-x-3">
+                  <Mail className="w-6 h-6 text-blue-500" />
+                  <div>
+                    <p className="text-sm font-medium text-gray-500 dark:text-gray-400">Email</p>
+                    <p className="text-base text-gray-900 dark:text-white">sedigojs@gmail.com</p>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-3">
+                  <Cake className="w-6 h-6 text-purple-500" />
+                  <div>
+                    <p className="text-sm font-medium text-gray-500 dark:text-gray-400">Date of Birth</p>
+                    <p className="text-base text-gray-900 dark:text-white">June 18, 2003</p>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-3">
+                  <MapPin className="w-6 h-6 text-red-500" />
+                  <div>
+                    <p className="text-sm font-medium text-gray-500 dark:text-gray-400">Location</p>
+                    <p className="text-base text-gray-900 dark:text-white">#308 GSIS Rd. GB1 San Mateo, Rizal 1850 Philippines</p>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-3">
+                  <Phone className="w-6 h-6 text-green-500" />
+                  <div>
+                    <p className="text-sm font-medium text-gray-500 dark:text-gray-400">Phone</p>
+                    <p className="text-base text-gray-900 dark:text-white">(+63) 928 681 9047</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="flex-1">
-            <h3 className="text-2xl font-semibold text-gray-900 dark:text-white mb-4">Joshua Sedigo</h3>
-            <p className="text-gray-600 dark:text-gray-300 mb-4">
-              I&apos;m a passionate web developer with 3 years of experience in front-end development and 1 year in back-end development, specializing in creating modern, responsive web applications. My expertise includes React, Next.js, Node.js, and a variety of other technologies within the JavaScript ecosystem.
-            </p>
-            <p className="text-gray-600 dark:text-gray-300 mb-4">
-              When I&apos;m not coding, I enjoy exploring new technologies and keeping up with the latest trends in the tech world, or enjoying hobbies like gaming, reading, and studying.
-            </p>
-            <h4 className="text-xl font-semibold text-gray-900 dark:text-white mt-8 mb-4">Skills</h4>
-            <ul className="grid grid-cols-2 gap-2">
-              {skills.map((skill) => (
-                <li key={skill} className="bg-gray-200 dark:bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-300">
-                  {skill}
-                </li>
-              ))}
-            </ul>
+          <div className="grid grid-cols-1 gap-12 md:gap-0">
+            <div className='space-y-4 mt-14'>
+              <h3 className="text-2xl font-semibold text-gray-900 dark:text-white mb-4">Joshua Sedigo</h3>
+              <p className="text-gray-600 dark:text-gray-300 mb-4">
+                I&apos;m a passionate web developer with 3 years of experience in front-end development and 1 year in back-end development, specializing in creating modern, responsive web applications. My expertise includes React, Next.js, Node.js, and a variety of other technologies within the JavaScript ecosystem.
+              </p>
+              <p className="text-gray-600 dark:text-gray-300 mb-4">
+                When I&apos;m not coding, I enjoy exploring new technologies and keeping up with the latest trends in the tech world, or enjoying hobbies like gaming, reading, and studying.
+              </p>
+            </div>
+            <div>
+              <h4 className="text-xl font-semibold text-gray-900 dark:text-white mt-8 mb-4">Skills</h4>
+              <ul className="grid grid-cols-2 sm:grid-cols-3 gap-2 mb-8">
+                {skills.map((skill) => (
+                  <li key={skill} className="bg-gray-200 dark:bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-300">
+                    {skill}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,7 +9,8 @@ const projects = [
   {
     id: 1,
     title: 'LMS x AAI',
-    description: 'An online learning management system where teachers or administrators can create and manage course modules, lessons, and quizzes for students. The platform allows students to access educational content, complete quizzes, and track their progress.',
+    type: '(Corporate Application)',
+    description: 'An online learning management system (LMS) where teachers or administrators can create and manage course modules, lessons, and quizzes for students. The platform allows students to access educational content, complete quizzes, and track their progress.',
     image: '/p1.png',
     github: 'https://github.com/SedigoJS/LMS',
     live: '/projects/p1',
@@ -17,6 +18,7 @@ const projects = [
   {
     id: 2,
     title: 'Surf x Bini',
+    type: '(Brand Campaign)',
     description: 'A content management system (CMS) website where users can submit entries, and an admin interface is used to review and validate these submissions. The admin site offers tools for reviewing, approving, or rejecting entries based on predefined criteria, ensuring that content meets the necessary standards. Once validated, the approved entries are forwarded to the brand responsible for awarding prizes.',
     image: '/p2.png',
     github: 'https://github.com/SedigoJS/Bini-Con',
@@ -25,6 +27,7 @@ const projects = [
   {
     id: 3,
     title: 'CloudCast',
+    type: '(Personal Project)',
     description: 'A weather dashboard integrates the OpenWeatherMap API to provide real-time weather updates, including current conditions such as temperature, humidity, and forecasts, alongside weather-related icons. Users can check the weather for their location or any city they choose. Additionally, the dashboard incorporates a music API, like Spotify or Apple Music.',
     image: '/p3.png',
     github: 'https://github.com/SedigoJS/CloudCast',
@@ -33,6 +36,7 @@ const projects = [
   {
     id: 4,
     title: 'Randomizer',
+    type: '(Corporate Application)',
     description: 'A tool similar to a Google Spin Wheel, allowing users to upload entries through files, such as CSVs, and randomly select winners. Users can easily input a list of participants or entries from a file, and the system randomly selects one or more winners from the list.',
     image: '/p4.png',
     github: 'https://github.com/SedigoJS/Randomizer',
@@ -67,7 +71,8 @@ export default function ProjectsGrid() {
             <div key={project.id} className="bg-gray-100 dark:bg-gray-700 rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105">
               <Image width={1000} height={1000} src={project.image} alt={project.title} className="w-full h-48 object-cover" />
               <div className="p-6">
-                <h3 className="text-xl font-semibold text-gray-900 dark:text-white mb-2">{project.title}</h3>
+                <h3 className="relative text-xl font-semibold text-gray-900 dark:text-white mb-2">{project.title}
+                <span className='absolute text-sm top-1 ml-2 font-normal text-gray-500 dark:text-gray-400'><i>{project.type}</i></span></h3>
                 <p className="text-gray-600 dark:text-gray-300 mb-4">{project.description}</p>
                 <div className="flex justify-between">
                   <Link href={project.github} target="_blank" rel="noopener noreferrer" className="flex items-center text-indigo-600 dark:text-indigo-400 hover:text-indigo-800 dark:hover:text-indigo-200">
